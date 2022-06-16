65a6b410a0899392194c1310dea61c82fd96262d
Leandro Costa <leandrin.costta@gmail.com>
2022-06-16 17:07:05 -0300

    Desafio for/while

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//Exercicio 1 -------------------------
+
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//Exercicio 2 -------------------------
+
+let contador = 10
+
+while (1 <= contador){
+    console.log(contador)
+    contador -=1
+}
+
+//Exercicio 3 -------------------------
+
+for(let number = 1; number <= 100; number +=2){
+   console.log(number)
+}
+
+//Exercicio 4 -------------------------
+
+for(let number = 0; number <= 100; number +=2){
+   console.log(number)
+}
